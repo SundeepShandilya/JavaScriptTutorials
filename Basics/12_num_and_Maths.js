4e42707c650c042f84f33933d3dc0e3d4882620e
SundeepShandilya <134867658+SundeepShandilya@users.noreply.github.com>
2024-06-07 16:24:35 +0000

    conv save

---
+++ Basics/12_num_and_Maths.js
@@ -0,0 +1,18 @@
+const num=50000000;
+
+const nm= new Number(600);
+
+console.log(num);
+console.log(nm);
+
+console.log(nm.toFixed(2));
+
+const fnum=12.879;
+console.log(fnum.toPrecision(3));
+
+console.log(num.toLocaleString('en-IN'))
+
+//************MAths************ /
+
+console.log(Math)
+console.log(Math.abs(-7))
