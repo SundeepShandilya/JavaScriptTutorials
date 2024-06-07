d13b0b9a957ca3adb3a841fff4bb9512af174d59
SundeepShandilya <134867658+SundeepShandilya@users.noreply.github.com>
2024-06-07 16:46:02 +0000

    conv save

---
+++ Basics/DateinJs.js
@@ -0,0 +1,15 @@
+
+let myDate= new Date();
+
+console.log(myDate)
+
+console.log(myDate.toString());//Coordinated Universal Time shows Time
+
+console.log(myDate.toDateString());//Shows only day and Date
+
+console.log(myDate.toISOString());//Some Entirely different Format with z
+
+console.log(myDate.toLocaleString()) //6/7/2024, 4:37:17 PM
+
+console.log(myDate.toTimeString());//16:38:08 GMT+0000 (Coordinated Universal Time)
+
