f65b7b938aca207a17cf96d9ed6d174552714404
SundeepShandilya <134867658+SundeepShandilya@users.noreply.github.com>
2024-06-07 10:21:11 +0000

    Conv Change

---
+++ Basics/03_ConversionOperations.js
@@ -0,0 +1,14 @@
+let a=true
+
+console.log(typeof a)
+
+let b=Number(a)
+console.log(typeof b)
+console.log(b)
+
+
+/*
+"33"=>33
+"abc454" => Nan
+True => 1
+*/
---
+++ Basics/Vairiables.js
@@ -0,0 +1,4 @@
+let a=3545
+const b="akdf"
+
+console.table([a,b])
